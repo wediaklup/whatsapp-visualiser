cf18344237f68ec163bd44b3ede4243e9b020002
wediaklup <wediaklup@protonmail.ch>
2024-08-08 18:46:43 +0200

    chats sind jetzt gutaussehend (cursed programming)

--- __init__.py
+++ __init__.py
@@ -14,22 +14,63 @@ class Message:
         self.author = author
         self.sent = sent
 
+    @staticmethod
+    def is_system():
+        return False
+
 
 class SystemMessage:
-    def __init__(self, content):
+    def __init__(self, content, sent: datetime):
         self.content = content
+        self.sent = sent
+
+    @staticmethod
+    def is_system():
+        return True
 
 
 def _parsearr(arr: list):
     output = []
-    for line in arr:
+    for index, line in enumerate(arr):
         try:
             raw_datetime, raw_name_content = line.split("-", 1)
             sent = datetime.strptime(raw_datetime.strip(), "%d.%m.%y, %H:%M")
+
+            if raw_name_content.strip().startswith("\u200e"):
+                output.append(SystemMessage(raw_name_content.strip(), sent))
+
             author, message = raw_name_content.split(":", 1)
+
+            # traversing following lines to see if they are just new lines in the same message
+            i = index + 1
+            while True:
+                try:
+                    # does the line follow the syntax of a new message?
+                    child_raw_datetime, child_raw_name_content = arr[i].split("-", 1)
+                    _ = datetime.strptime(child_raw_datetime.strip(), "%d.%m.%y, %H:%M")
+                    _, child_message = child_raw_name_content.split(":", 1)
+                    # print(f"({i}) breaking out from new message line")
+                    break
+                except ValueError:
+                    # if it doesn’t, it’s either a system message or a lew line
+                    # print(f"({i}) discovered new orphaned line")
+                    try:
+                        # is it a system message?
+                        child_raw_datetime, child_content = arr[i].split("-", 1)
+                        _ = datetime.strptime(child_raw_datetime.strip(), "%d.%m.%y, %H:%M")
+                        # print(f"({i}) discovered new system message")
+                        break
+                    except ValueError:
+                        # so it must be a new line that we append to the previous message
+                        # print(f"({i}) discovered new message line")
+                        message += "\n" + arr[i].strip()
+                        i += 1
+                except IndexError:
+                    break
+
             output.append(Message(message.strip(), author.strip(), sent))
         except ValueError:
-            output.append(SystemMessage(line.strip()))
+            pass
 
     return output
 
@@ -41,6 +82,10 @@ def parse(fp, encoding="utf-8"):
 
 def parses(text: str):
     return _parsearr(text.split("\n"))
+
+
+def get_messages():
+    return parses(session.get("messages"))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -55,9 +100,15 @@ def root():
 
 @app.route("/chat")
 def chat():
-    messages = session.get("messages")
+    messages = get_messages()
+    pov = "Rafael"
 
-    return render_template("chat.html", messages=messages)
+    return render_template("chat.html", messages=enumerate(messages), pov=pov)
+
+
+@app.route("/test")
+def test():
+    return render_template("test-chat.html")
 
 
 @app.route("/clear")
